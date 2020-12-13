c4d5d306f6cbd9690d42ed3f9953d748a2f4eac7
chintalapally sandeep reddy <chsandeep79@gmail.com>
2020-12-13 14:04:58 -0600

    Add files via upload

---
+++ UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ShellScript.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void When_Sum_Two_Numbers_returns_Positive()
+        {
+            var x = 10;
+            var y = 20;
+            Assert.IsTrue(x + y > 0);
+        }
+    }
+}
